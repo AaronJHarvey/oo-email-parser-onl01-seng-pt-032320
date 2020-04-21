03e8a71076aeedfe3c530fe251ace59057b8715b
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-21 00:37:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -10,6 +10,12 @@ def initialize(csv_emails)
   @csv_emails = csv_emails
 end
 
-def parse 
+def parse
+  csv_emails.split.collect do |address|
+    address.split(',')
+  end
+  .flatten.uniq
+end
+end
 
 end
